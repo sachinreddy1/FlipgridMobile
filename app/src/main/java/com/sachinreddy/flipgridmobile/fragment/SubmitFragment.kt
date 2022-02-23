f30d4a03e66fe220fde2a21fc5194e0475b97db5
sachinreddy1 <reddysachin2014@gmail.com>
2022-02-22 22:14:19 -0500

    Added styling, fonts, and backgrounds.

--- app/src/main/java/com/sachinreddy/flipgridmobile/fragment/SubmitFragment.kt
+++ app/src/main/java/com/sachinreddy/flipgridmobile/fragment/SubmitFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.sachinreddy.flipgridmobile.databinding.FragmentSubmitBinding
 import com.sachinreddy.flipgridmobile.viewmodel.MainViewModel
 
 class SubmitFragment: Fragment() {
+    private var binding: FragmentSubmitBinding? = null
     private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -20,7 +22,16 @@ class SubmitFragment: Fragment() {
         FragmentSubmitBinding.inflate(inflater, container, false).apply {
             lifecycleOwner = this@SubmitFragment
             vm = mainViewModel
+            binding = this
             return root
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding?.buttonSignIn?.setOnClickListener {
+            Toast.makeText(context, "Signing in...", Toast.LENGTH_SHORT).show()
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 }
